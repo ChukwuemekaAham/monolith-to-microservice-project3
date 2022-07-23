88e9c2e16045ac79626ebba14e8c52243dd8efa4
chukwuemeka <ahamchukwuemeka2@gmail.com>
2022-07-23 12:58:08 +0100

    Updated dev host

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a48d12137c6344f43a5cc9be212d9a37-2020847628.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
